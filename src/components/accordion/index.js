5ceb714d2ff07825568204c9a874161d227e242a
tristanhamel <tristan.hamel@kielikone.fi>
2017-05-17 22:52:50 +0300

    better accordion

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -4,28 +4,25 @@ export const accordionComponent = {
   controller: () => {
     $( document ).ready(() => {
       // add listeners to accordion headers
-        $('[th-accordion-header]').click(onClickHeader);
+        $('[th-accordion-section]').click(onClick);
 
-      function onClickHeader(e) {
-        const clickedHeader = $(e.currentTarget);
-        const parentAccordion = $(e.currentTarget).parents('[th-accordion]');
-        const headerSections = parentAccordion.find('[th-accordion-header]');
-        const contentSections = parentAccordion.find('[th-accordion-content]');
-        const nextContent = $(e.currentTarget).next('[th-accordion-content]');
+      function onClick(e) {
+        const clickedSection = $(e.currentTarget);
+        // check if it is an opened section
+        const isExpanded = clickedSection.hasClass('accordion-expanded');
 
+        clickedSection
+          .parents('[th-accordion]')
+          .find('.accordion-expanded')
+          .removeClass('accordion-expanded')
+          .find('[th-accordion-content]')
+          .slideUp(300);
 
-        // check if the the clicked header is that of an opened section
-        const isExpanded = clickedHeader.hasClass('accordion-expanded');
-
-        // start by collapsing visible content sections...
-        $(headerSections).removeClass('accordion-expanded');
-        $(contentSections).removeClass('accordion-expanded');
-        $(contentSections).slideUp(500);
-
-        // ... and then expand the clicked element if it was collapsed
         if (!isExpanded) {
-          $(clickedHeader).addClass('accordion-expanded');
-          $(nextContent).addClass('accordion-expanded').slideDown(500);
+          clickedSection
+            .addClass('accordion-expanded')
+            .find('[th-accordion-content]')
+            .slideDown(300);
         }
       }
     });
