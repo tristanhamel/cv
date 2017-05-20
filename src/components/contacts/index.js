04d4a6ada5b2ff17a845211794d6d71f9af48be4
tristanhamel <tristan.hamel@kielikone.fi>
2017-05-20 10:36:45 +0300

    integrate contacts in menu

--- src/components/contacts/index.js
+++ src/components/contacts/index.js
@@ -4,30 +4,5 @@ import './contacts.scss';
 
 export const contactsComponent = {
   selector: 'th-contacts',
-  template,
-  controller: () => {
-    const introHeight = 500;
-    const contactElement = $('.contacts');
-
-    $(document).ready(() => {
-      let allow = true;
-      $(window).scroll((e) => {
-        if(!allow) {
-          return;
-        }
-        if(e.currentTarget.scrollY > introHeight) {
-          contactElement.addClass('fixed');
-        } else {
-          contactElement.removeClass('fixed');
-        }
-
-        allow = false;
-        window.setTimeout(() => {allow = true;}, 200);
-      });
-    });
-
-    $('.contacts-toggle').click(() => {
-      contactElement.toggleClass('visible');
-    });
-  }
+  template
 };
